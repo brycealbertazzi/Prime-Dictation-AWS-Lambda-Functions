e57462ba16f719c59d35c0d056314a4232b29636
Bryce Albertazzi <brycealbertazzi@gmail.com>
2025-10-28 12:05:57 -0600

    update dependencies to commands work

--- lambdas/pd-email-sender/index.mjs
+++ lambdas/pd-email-sender/index.mjs
@@ -3,7 +3,7 @@
 import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 import { S3Client, GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { buildHtml, buildText } from "pd-shared";
+import { buildHtml, buildText } from "../../shared/src/index.js";
 
 /**
  * Required environment variables (configure in Lambda console):
